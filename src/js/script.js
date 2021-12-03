d7612220a659980d7194a45c831df1d8c2a562bd
Danil_Rybkin <danil.rybkin.19@gmail.com>
2021-12-03 20:22:09 +0300

    add tiny-slider

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    autoplay: false,
+    nav: false,
+    controlsText: [
+        '<img src="icons/left.svg"></img>',
+        '<img src="icons/right.svg"></img>'
+    ]
+});
