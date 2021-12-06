946e924e5015d8af933fcc15b636abc9f3c6dea4
Danil_Rybkin <danil.rybkin.19@gmail.com>
2021-12-06 22:58:30 +0300

    add validate for form

--- src/js/script.js
+++ src/js/script.js
@@ -42,3 +42,34 @@ $('.button_mini').each(function(i) {
         $('.overlay, #order').fadeIn('slow');  
     })    
 });
+
+function validateForms(form) {
+    $(form).validate({
+        rules: {
+            name: {
+                required: true,
+                minlength: 2
+            },
+            phone: "required",
+            email: {
+                required: true,
+                email: true 
+            }
+        },
+        messages: {
+            name: {
+                required: "Пожалуйста, введите своё имя",
+                minlength: jQuery.validator.format("Введите более {0} символов!")
+            },
+            phone: "Пожалуйста, введите свой номер телефона",
+            email: {
+                required: "Пожалуйста, введите свою почту",
+                email: "Неправильно введен адрес почты"
+            }   
+        }
+    });    
+}
+
+validateForms('#consultation-form');
+validateForms('#consultation form');
+validateForms('#order form');
